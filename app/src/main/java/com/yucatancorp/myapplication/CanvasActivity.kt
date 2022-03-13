88884e8186ce99a3da15e9b5a60baa8c5eb73920
MarcosArmando <narvaezaldana@gmail.com>
2022-03-12 18:14:50 -0600

    create canvas field

--- app/src/main/java/com/yucatancorp/myapplication/CanvasActivity.kt
+++ app/src/main/java/com/yucatancorp/myapplication/CanvasActivity.kt
@@ -3,13 +3,10 @@ package com.yucatancorp.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CanvasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_canvas)
     }
 
-    public fun chekData(){
-
-    }
 }
